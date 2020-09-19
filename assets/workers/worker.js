c298412a0faf4139928c949d0ad3f65d98b42da2
jgw9617 <jgw9617@gmail.com>
2020-09-18 22:28:51 -0700

    completely offscreen

--- assets/workers/worker.js
+++ assets/workers/worker.js
@@ -4,84 +4,16 @@ importScripts("/assets/filters/webgl.js");
 // importScripts("../../../dist/umd/comlink.js");
 
 const obj = {
-  counter: 0,
   offscreen: new OffscreenCanvas(40, 40),
   filter: new WebGLImageFilter(),
 
-  inc() {
-    this.counter++;
-  },
-
-  blackAndWhite(d, width, height) {
-    for (var i = 0; i < d.length; i += 4) {
-      let r = d[i];
-      let g = d[i + 1];
-      let b = d[i + 2];
-      // CIE luminance for the RGB
-      // The human eye is bad at seeing red and blue, so we de-emphasize them.
-      let v = 0.2126 * r + 0.7152 * g + 0.0722 * b;
-      d[i] = d[i + 1] = d[i + 2] = v
+  async doWebGL(type, canvasImage, width, height, amount) {
+    if (amount) {
+      this.filter.addFilter(type, amount);
     }
-
-    let image = new ImageData(d, width, height);
-
-    return image;
-  },
-
-  invert(d, width, height) {
-    for (var i = 0; i < d.length; i += 4) {
-      var r = d[i];
-      var g = d[i + 1];
-      var b = d[i + 2];
-      var v = (0.2126 * r + 0.7152 * g + 0.0722 * b >= 90) ? 255 : 0;
-      d[i] = d[i + 1] = d[i + 2] = v
+    else {
+      this.filter.addFilter(type);
     }
-
-    let image = new ImageData(d, width, height);
-
-    return image;
-  },
-
-  enhance(d, width, height) {
-    const adjustment = 40;
-
-    for (var i = 0; i < d.length; i += 4) {
-      d[i] += adjustment;
-      d[i + 1] += adjustment;
-      d[i + 2] += adjustment;
-    }
-
-    let image = new ImageData(d, width, height);
-
-    return image;
-  },
-
-  saturate(d, width, height) {
-    const amount = 2;
-
-    for (let i = 0; i < d.length; i += 4) {
-      let r = (.213 + .787 * amount) * d[i]
-        + (.715 - .715 * amount) * d[i + 1]
-        + (.072 - .072 * amount) * d[i + 2];
-      let g = (.213 - .213 * amount) * d[i]
-        + (.715 + .285 * amount) * d[i + 1]
-        + (.072 - .072 * amount) * d[i + 2];
-      let b = (.213 - .213 * amount) * d[i];
-      + (.715 - .715 * amount) * d[i + 1]
-        + (.072 + .928 * amount) * d[i + 2];
-
-      d[i] = r;
-      d[i + 1] = g;
-      d[i + 2] = b;
-    }
-
-    let image = new ImageData(d, width, height);
-
-    return image;
-  },
-
-  async doWebGL(type, canvasImage, width, height) {
-    this.filter.addFilter(type);
 
     this.offscreen.width = width;
     this.offscreen.height = height;
@@ -99,36 +31,15 @@ const obj = {
     return blobToDraw;
   },
 
-  async doAI(canvasImage) {
-    const splitData = canvasImage.split(',')[1];
+  async getBlob(imageData, width, height) {
+    this.offscreen.width = width;
+    this.offscreen.height = height;
 
-    const bytes = self.atob(splitData);
-    const buf = new ArrayBuffer(bytes.length);
-    let byteArr = new Uint8Array(buf);
+    offscreenContext = this.offscreen.getContext("2d");
 
-    for (var i = 0; i < bytes.length; i++) {
-      byteArr[i] = bytes.charCodeAt(i);
-    }
+    offscreenContext.drawImage(imageData, 0, 0, width, height);
 
-    let data = null;
-
-    try {
-      const response = await fetch(`https://westus2.api.cognitive.microsoft.com/vision/v3.0/generateThumbnail?width=400&height=450&smartCropping=true`, {
-        headers: {
-          "Ocp-Apim-Subscription-Key": "d930861b5bba49e5939b843f9c4e5846",
-          "Content-Type": "application/octet-stream"
-        },
-        method: "POST",
-        body: byteArr
-      });
-      data = await response.blob();
-
-      return data;
-
-    } catch (error) {
-      console.error(error);
-      return error;
-    }
+    return this.offscreen.convertToBlob();
   }
 };
 // @ts-ignore
