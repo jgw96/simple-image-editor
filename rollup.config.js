d7f4232cecfdaa87bff78e7188d43da5f663f192
jgw9617 <jgw9617@gmail.com>
2020-10-24 17:04:28 -0700

    chore(): get around bug in azure

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,13 @@ import resolve from 'rollup-plugin-node-resolve';
 import { terser } from "rollup-plugin-terser";
 import { injectManifest } from 'rollup-plugin-workbox';
 import html from '@open-wc/rollup-plugin-html';
-import strip from '@rollup/plugin-strip';
 import copy from 'rollup-plugin-copy';
 import wasm from '@rollup/plugin-wasm';
 
 export default {
   input: 'index.html',
   output: {
-    dir: 'dist',
+    dir: 'dist/app/',
     format: 'es',
   },
   plugins: [
@@ -19,16 +18,16 @@ export default {
     terser(),
     copy({
       targets: [
-        { src: 'assets/**/*', dest: 'dist/assets/' },
-        { src: 'styles/global.css', dest: 'dist/styles/'},
-        { src: 'manifest.json', dest: 'dist/'},
-        { src: '.well-known/*', dest: 'dist/.well-known/' },
+        { src: 'assets/**/*', dest: 'dist/app/assets/' },
+        { src: 'styles/global.css', dest: 'dist/app/styles/'},
+        { src: 'manifest.json', dest: 'dist/app/'},
+        { src: '.well-known/*', dest: 'dist/app/.well-known/' },
         { src: 'routes.json', dest: 'dist/' },
       ]
     }),
     injectManifest({
       swSrc: 'pwabuilder-sw.js',
-      swDest: 'dist/pwabuilder-sw.js',
+      swDest: 'dist/app/pwabuilder-sw.js',
       globDirectory: 'dist/',
       globPatterns: [
         'styles/*.css',
